6d4b0c58b0808f0124c0bfc64c1dc63f27e65b3c
Gandayiki <gelayegeremew@gmail.com>
2022-06-16 00:10:11 +0300

    all_check.py added to check the reboot pending and disk usage of the device

---
+++ all_checks.py
@@ -0,0 +1,33 @@
+import os
+import shutil
+import sys
+
+
+def Reboot():
+    return os.path.exists('/run/reboot-required')
+
+def check_disk_full(disk,min_gb,min_percent):
+    du  = shutil.disk_usage(disk)
+    percent_free = 100 * du.free / du.total
+    gigabyte_free = du.free / 2**30
+    
+    if percent_free < min_percent or gigabyte_free < min_gb:
+        return True
+    return False
+    
+    
+def main():
+    if Reboot():
+        print('Computer is reboot seted')
+        sys.exit(1)
+    print('Reboot is Not Pending ...')
+    
+    if check_disk_full('/',2,10):
+        print('Disk Full')
+        sys.exit(1)
+    
+    print('Every Thing is Ok!')
+    sys.exit(0)
+    
+
+main()
